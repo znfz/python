c5158cdbf62b04f38e5bdeaa1ca8432c2c5f33f2
znfz <123204391+znfz@users.noreply.github.com>
2023-03-29 23:27:26 -0400

    Update modeldevelopment.py

--- modeldevelopment.py
+++ modeldevelopment.py
@@ -1,69 +1,157 @@
-#1. Develop a linear model between 'peak-rpm' vs. 'price'
+#1. Create a MLR model 
+
 import pandas as pd
+import numpy as np
 df=pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv')
-x=df[['peak-rpm']]
-x1=df['peak-rpm']
+x=df[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg','normalized-losses','symboling']]
 y=df['price']
 
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 lm=LinearRegression()
 lm.fit(x,y)
-yhat=lm.predict(x)
+yhat=np.round(lm.predict(x),1)
+print('Predicted values are',yhat[:3])
+print('Actual values are',y[:3].tolist())
+print('R2:',np.round(lm.score(x,y),4))
+print('The gradients are',np.round(lm.coef_,1))
+print('The intercept is',np.round(lm.intercept_,1))
 import matplotlib.pyplot as plt
-plt.plot(x1,yhat,color='red')
-plt.scatter(x1,y)
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
-print('Gradients:',lm.coef_)
-print('Intercept:',lm.intercept_)
-print('R2:',lm.score(x,y))
-print('MSE:',mean_squared_error(y,yhat))
+x_range=range(len(x))
+plt.plot(x_range,y,color='red')
+plt.scatter(x_range,yhat)
+plt.xlabel('#')
+plt.ylabel('Price')
 
-#2. Develop a polynomial model between 'peak-rpm' vs. 'price' 
+#2.Do a cross_val 3 times
+from sklearn.model_selection import cross_val_score
+cvs=np.round(cross_val_score(lm,x,y,cv=3),3)
+print('R2:',cvs)
+from sklearn.model_selection import cross_val_predict
+yhat=np.round(cross_val_predict(lm,x,y,cv=3),3)
+print('Predicted',yhat[:3])
+print('Actual',(y[:3].tolist()))
+
+#3. Create a MLR with 40% test sample
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=0)
+from sklearn.linear_model import LinearRegression
+lm=LinearRegression()
+lm.fit(x_train,y_train)
+yhat=np.round(lm.predict(x_test),1)
+
+print('Predicted values are',yhat[:3])
+print('Actual values are',y_test[:3].tolist())
+print('R2:',np.round(lm.score(x_test,y_test),3))
+print('The gradients are',np.round(lm.coef_,3))
+print('The intercepts are',np.round(lm.intercept_,1))
+import matplotlib.pyplot as plt
+x_num=range(len(x_test))
+plt.plot(x_num,y_test,color='red')
+plt.scatter(x_num,y_test)
+plt.xlabel('#')
+plt.ylabel('Price')
+
+#4. Create a PR with 40% test sample with degree=2
+
 import pandas as pd
 df=pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv')
-x=df[['peak-rpm']]
-x1=df['peak-rpm']
+x=df[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg']]
+y=df['price']
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=0)
+
+from sklearn.preprocessing import PolynomialFeatures
+pm=PolynomialFeatures(degree=2)
+x_trainp=pm.fit_transform(x_train)
+x_testp=pm.fit_transform(x_test)
+
+from sklearn.linear_model import LinearRegression
+lm=LinearRegression()
+lm.fit(x_trainp,y_train)
+yhat=np.round(lm.predict(x_testp),1)
+R2=np.round(lm.score(x_testp,y_test),3)
+print('Predicted values are',yhat[:3].tolist())
+print('Actual values are',y_test[:3].tolist())
+print('The gradients are',lm.coef_)
+print('The intercept is',np.round((lm.intercept_),2))
+print('R2:',R2)
+
+x_num=range(len(x_test))
+import matplotlib.pyplot as plt
+plt.scatter(x_num,y_test)
+plt.plot(x_num,yhat,color='red')
+plt.xlabel('#')
+plt.ylabel('Price')
+
+#6. Create PM of degree=5
+
+import pandas as pd
+import numpy as np
+df=pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv')
+x=df[['horsepower']]
+y=df['price']
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=0)
+
+from sklearn.preprocessing import PolynomialFeatures
+pm=PolynomialFeatures(degree=5)
+x_trainp=pm.fit_transform(x_train)
+x_testp=pm.fit_transform(x_test)
+
+from sklearn.linear_model import LinearRegression
+lm=LinearRegression()
+lm.fit(x_trainp,y_train)
+yhat=lm.predict(x_testp)
+
+import matplotlib.pyplot as plt
+plt.scatter(x_test,y_test)
+plt.scatter(x_test,yhat)
+plt.xlabel('Price')
+plt.ylabel('Horsepower')
+int=np.round(lm.intercept_,1)
+print('Gradients:',lm.coef_)
+print('Intercept',int)
+R2=np.round(lm.score(x_testp,y_test),3)
+print('R2:',R2)
+
+#7. Develop a Polynomial model
+
+import pandas as pd
+import numpy as np
+df=pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv')
+x=df[['horsepower', 'curb-weight', 'engine-size', 'highway-mpg','normalized-losses','symboling']]
 y=df['price']
 
 from sklearn.preprocessing import PolynomialFeatures
 pm=PolynomialFeatures(degree=2)
-x_poly=pm.fit_transform(x)
-from sklearn.linear_model import LinearRegression
-lm=LinearRegression()
-lm.fit(x_poly,y)
 
-yhat=lm.predict(x_poly)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=0)
 
+x_trainp=pm.fit_transform(x_train)
+x_testp=pm.fit_transform(x_test)
+
+from sklearn.linear_model import Ridge
+
+rm=Ridge(alpha=10000000000)
+rm.fit(x_trainp,y_train)
+yhat=np.round(rm.predict(x_testp),1)
+#print('Predicted:',yhat[:3].tolist())
+#print('Actual:',y_test[:3].tolist())
+
+R2_te=rm.score(x_testp,y_test)
+R2_tr=rm.score(x_trainp,y_train)
+print('R2 for testing:',np.round(R2_te,3))
+print('R2 for training:',np.round(R2_tr,3))
+
+x_num=range(len(x_test))
 import matplotlib.pyplot as plt
-plt.plot(x1,yhat,color='red')
-plt.scatter(x1,y,)
+plt.scatter(x_num,y_test)
+plt.plot(x_num,yhat,color='red')
+plt.xlabel('#')
+plt.ylabel('Price')
 
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
-print('R2',r2_score(y,yhat))
-print('MSE:',mean_squared_error(y,yhat))
-print('Gradient:',lm.coef_)
-print('Intercept:',lm.intercept_)
-
-#3. Develop a Multiple Linear Regression for price
-import pandas as pd
-df=pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv')
-z=df[['horsepower','curb-weight','engine-size','highway-mpg']]
-y=df['price']
-
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
-lm=LinearRegression()
-lm.fit(z,y)
-
-yhat=lm.predict(z)
-print('Gradient:',lm.coef_)
-print('Intercept',lm.intercept_)
-print('R2:',r2_score(y,yhat))
-print('MSE:',mean_squared_error(y,yhat))
-
-import matplotlib.pyplot as plt
-plt.plot(y)
-plt.plot(yhat)
+#All these models should be used in consideration when developing predictive models.
